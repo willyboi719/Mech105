48a19fe1929c36a91012be2f080aab44d9a4118f
willyboi719 <132632385+willyboi719@users.noreply.github.com>
2023-05-04 20:34:56 -0600

    Update NewtonRaphsonMethod.m
    Uses the newton-raphson method to approximate the root of a given function.

--- NewtonRaphsonMethod.m
+++ NewtonRaphsonMethod.m
@@ -1,9 +1,9 @@
-
-
-old = 3.2;               %Initial Guess
-eaf = .1; %Desired approx error
 syms x
 g(x) =  tanh(x^2-9); %function that you are evaluating
+initialguess = 3.2;               %Initial Guess
+
+eaf = .1; %Desired approx error
+
 gprime(x) = diff(g);
 
 ea = 1;
@@ -11,7 +11,7 @@ p = 0;
 
 while p < 3      %Number of iterations times
    
-    new = old - (g(old)) / (gprime(old));
+    new = initialguess - (g(initialguess)) / (gprime(initialguess));
   % ea = abs(x0 - root);
    old = vpa(new)
 p = p+1;
