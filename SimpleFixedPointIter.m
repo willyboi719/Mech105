432a41fcfed9d09d02532f27f9f64ae5a20902bc
willyboi719 <132632385+willyboi719@users.noreply.github.com>
2023-05-04 20:36:00 -0600

    Update SimpleFixedPointIter.m
    Uses simple fixed point iteration to approximate the root of a given function.

--- SimpleFixedPointIter.m
+++ SimpleFixedPointIter.m
@@ -1,7 +1,9 @@
-x0 = 5.5;               %Initial Guess
-eaf = .0001; %Desired approx error
 syms x
 g = @(x) x^3 + 6*x^2 +11*x -6.1; %function that you are evaluating
+x0 = 5.5;               %Initial Guess
+eaf = .0001; %Desired approx error
+
+
 ea = 1;
 while ea > eaf
    root = g(x0);
